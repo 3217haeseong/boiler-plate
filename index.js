5dcad00f8bb27296bdfc0a8cd99b2bb56e361c6c
haeseong <gotjd3546@gmail.com>
2021-06-05 15:04:05 +0900

    비밀 정보 보호2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app=express()
 const port=5000
 const bodyParser=require("body-parser");
 const {User}=require("./models/User");
-
+const config=require('./config/key')
 
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended:true}));
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 const mongoose=require('mongoose')
-mongoose.connect('mongodb+srv://sana:abcd1234@boiler-plate.i6uas.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true,useUnifiedTopology:true, useCreateIndex:true,useFindAndModify:false
 }).then(()=>console.log('MongoDB Connected...'))
   .catch(err=>console.log(err))
