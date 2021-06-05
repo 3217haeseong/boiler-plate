e7ba3f6ca64c551d00faf36ea20971e82b59861a
haeseong <gotjd3546@gmail.com>
2021-06-05 14:35:24 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express=require('express')
 const app=express()
 const port=5000
+const bodyParser=require("body-parser");
+const {User}=require("./models/User");
 
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose=require('mongoose')
 mongoose.connect('mongodb+srv://sana:abcd1234@boiler-plate.i6uas.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
@@ -9,6 +17,21 @@ mongoose.connect('mongodb+srv://sana:abcd1234@boiler-plate.i6uas.mongodb.net/myF
 }).then(()=>console.log('MongoDB Connected...'))
   .catch(err=>console.log(err))
 
-  app.get('/',(req,res)=>res.send('Hello World!'))
+  app.get('/',(req,res)=>res.send('Hello World!~~~~'))
+
+  app.post('/register',(req,res)=>{
+
+    //회원가입할 때 필요한 정보들을 client에서 가져오면
+    //그것들을 데이터 베이스에 넣어준다.
+
+    const user=new User(req.body)
+
+    user.save((err,userInfo)=>{
+      if(err) return res.json({success:false,err})
+      return res.status(202).json({
+        success:true
+      })
+    })
+  })
 
   app.listen(port,()=>console.log(`Example app listening on port ${port}!`))
